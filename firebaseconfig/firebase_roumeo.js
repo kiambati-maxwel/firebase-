f5c982d39ffefcc8180abfa6fc17b4f220940a49
kiambati-maxwel <antidoteprog@gmail.com>
2021-04-07 21:47:15 +0300

    commit 1

---
+++ firebaseconfig/firebase_roumeo.js
@@ -0,0 +1,23 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.3.1/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/8.3.1/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyBFx74XvLVlYKAfeiBO4Q8WdqvLguiuek4",
+    authDomain: "roumeo-e9d79.firebaseapp.com",
+    projectId: "roumeo-e9d79",
+    storageBucket: "roumeo-e9d79.appspot.com",
+    messagingSenderId: "735194890738",
+    appId: "1:735194890738:web:ba73da6f90c203f2f1ab6b",
+    measurementId: "G-L16ZFG93VY"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
